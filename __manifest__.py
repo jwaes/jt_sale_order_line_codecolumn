b899f16b36194352e152456140ad8953f29e4a57
jwaes <jaco.waes@gmail.com>
2022-05-24 12:38:48 +0000

    remove code from description

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '15.0.1.0.1',
+    'version': '15.0.1.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['sale'],
