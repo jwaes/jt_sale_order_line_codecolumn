f159b1aaa5c484c73da187785291bcb6a4a82856
jwaes <jaco.waes@gmail.com>
2022-05-30 12:46:04 +0000

    added short name

--- models/product.py
+++ models/product.py
@@ -1,14 +1,24 @@
+import re
 from odoo import api, fields, models, _
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
+
+    short_name = fields.Char(compute='_compute_short_name', string='short_name')
+    
+    @api.depends('display_name')
+    def _compute_short_name(self):
+        for record in self:
+            result = re.search('^(\[.*\]\s)*(.*)$', record.display_name)
+            short = result.group(2)
+            record.short_name = short    
 
     def get_product_multiline_description_sale(self):
         """ Compute a multiline description of this product, in the context of sales
                 (do not use for purchases or other display reasons that don't intend to use "description_sale").
             It will often be used as the default description of a sale order line referencing this product.
         """
-        name = self.name
+        name = self.short_name
         if self.description_sale:
             name += '\n' + self.description_sale
 
---
+++ models/product_template.py
@@ -0,0 +1,14 @@
+import re
+from odoo import api, fields, models, _
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    short_name = fields.Char(compute='_compute_short_name', string='short_name')
+    
+    @api.depends('display_name')
+    def _compute_short_name(self):
+        for record in self:
+            result = re.search('^(\[.*\]\s)*(.*)$', record.display_name)
+            short = result.group(2)
+            record.short_name = short
