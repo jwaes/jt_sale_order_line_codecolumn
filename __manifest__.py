7c3d82af207b2d8f367e0980fdd76ee02399dd8a
jwaes <jaco.waes@gmail.com>
2024-06-13 08:18:57 +0000

    v17

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '0.4',
+    'version': '2.0',
 
     # any module necessary for this one to work correctly
     'depends': ['sale'],
